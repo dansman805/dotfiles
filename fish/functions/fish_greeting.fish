0b73de33e6256f54307f5a16f0c8d4ed8d4bffa0
Daniel Goz <dansman805@gmail.com>
2020-06-21 02:48:53 -0400

    Fix issue with random greeting where sometimes it would select the header of cowsay -l

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 function fish_greeting
-    set cow_to_use (cowsay -l | tr -s " " "\012" | shuf -n 1)
+    set cow_to_use (cowsay -l | tail -n +2 | tr -s " " "\012" | shuf -n 1)
     cowsay -f $cow_to_use (fortune)
 end
