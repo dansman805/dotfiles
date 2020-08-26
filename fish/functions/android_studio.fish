606aa3a709036f1c9c2a8b2974516b9750dbf120
Daniel Goz <dansman805@gmail.com>
2020-08-25 23:41:44 -0400

    Add android_studio function to fish
    because environment.d doesn't work for some reason

---
+++ fish/functions/android_studio.fish
@@ -0,0 +1,4 @@
+function android_studio
+    export _JAVA_AWT_WM_NONREPARENTING=1
+    /var/lib/flatpak/app/com.google.AndroidStudio/x86_64/stable/active/files/extra/android-studio/bin/studio.sh
+end
