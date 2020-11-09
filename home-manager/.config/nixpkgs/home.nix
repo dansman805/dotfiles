11b1f11c53ef42e7d8a13de5c6cb09f696137bf5
dansman805 <dansman805@gmail.com>
2020-11-09 08:25:44 -0500

    Add doom emacs to home-manager

--- home-manager/.config/nixpkgs/home.nix
+++ home-manager/.config/nixpkgs/home.nix
@@ -1,25 +1,36 @@
 {pkgs, ...}:
 
-{
+
+let
+  doom-emacs = pkgs.callPackage (builtins.fetchTarball {
+    url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
+  }) {
+    doomPrivateDir = ~/dotfiles/doom/.config/doom;
+    # Directory containing your config.el init.el                             # and packages.el files
+  };
+in {
   home.packages = with pkgs; [
     # GUI tools
     firefox kitty
     
     # General-purpose CLI tools
-    wget
+    wget ripgrep coreutils fd
 
     # Text editors
-    vim
+    doom-emacs vim
+	 
+    # Language-specific tools
+    clang
   ];
 
   programs.git = {
     enable = true;
     userName = "dansman805";
     userEmail = "dansman805@gmail.com";
-  };
+  };																																																						
 
-  programs.emacs = {
-    enable = true;
-  };
+  home.file.".emacs.d/init.el".text = ''
+      (load "default.el")
+  '';
 }
 
---
+++ nixos/device.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  networking.hostName = "nixos-testing-vm"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+}
