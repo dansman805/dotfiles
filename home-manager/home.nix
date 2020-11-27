6b52732144764fc49ac98c2e5fe5c2ef04facca7
Daniel Goz <dansman805@gmail.com>
2020-11-27 17:16:26 -0500

    home-manager: use to deploy git config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -34,6 +34,7 @@
     tealdeer
     ffmpeg
     imagemagick
+    nixfmt
 
     # Command line fun
     fortune
--- home-manager/xdg.nix
+++ home-manager/xdg.nix
@@ -1,16 +1,24 @@
 { pkgs, ... }:
 
+let
+  dotfilesDir = /home/dansman805/dotfiles;
+in
 { 
   xdg = {
     enable = true;
 
     configFile = {
       "doom" = {
-        source = "/home/dansman805/dotfiles/doom";
+        source = dotfilesDir + "/doom";
         recursive = true;
       };
 
-      "i3/config".source = /home/dansman805/dotfiles/i3/config;
+      "i3/config".source = dotfilesDir + "/i3/config";
+
+      "git" = {
+        source = dotfilesDir + "/git/";
+        recursive = true;
+      };
     };
   };
 }
