86c4dd78009305a7324ac2c131d5a7b51fdb9c3b
Daniel Goz <dansman805@gmail.com>
2021-02-26 12:12:52 -0500

    doom: switch to doom-flatwhite theme

--- doom/config.el
+++ doom/config.el
@@ -25,7 +25,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
+
+(setq doom-theme 'doom-flatwhite)
+;; for dark mode: doom-vibrant, alternate light mode doom-nord-light
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
